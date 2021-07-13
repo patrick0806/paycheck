27f74edee45b0655fbd7e7940d37bbba897ff0aa
Patrick da Silva Nicezi <patrickk0806@hotmail.com>
2021-07-13 16:13:17 -0300

    running with exit in my computer

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,63 @@
-import Head from 'next/head'
+import Head from "next/head";
+import {useForm} from "react-hook-form";
 
 export default function Home() {
+  const {register, handleSubmit} = useForm();
+  const onSubmit = async (data) =>{
+
+    const formData = new FormData();
+    formData.append("pdf",data.pdf[0]);
+    formData.append("excel",data.excel[0]);
+    
+    const res = await fetch(
+      'http://localhost:3005',
+      {
+        body: formData,
+        method: 'POST'
+      }
+    )
+
+    const response = await res.json();
+    if(response.message){
+      alert(response.message);
+    }else{
+      alert("Houve uma falha ao enviar os emails");
+    }
+
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="w-full h-screen bg-white flex flex-col items-center justify-center">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label className="my-2 block mb-1 text-sm" htmlFor="pdf">
+          Selecione o PDF
+        </label>
+        <input
+          id="pdf"
+        {...register('pdf',{required:true})}
+          type="file"
+          name="pdf"
+          className="my-2 block border shadow-inner w-full text-gray-700 p-1 border-gray-400"
+          required
+        />
+        <label className="my-2 block mb-1 text-sm" htmlFor="excel">
+          Selecione o Excel
+        </label>
+        <input
+          id="excel"
+          name="excel"
+          {...register('excel',{required:true})}
+          type="file"
+          className="my-2 block border shadow-inner w-full text-gray-700 p-1 border-gray-400"
+          required
+        />
+        <button
+          type="submit"
+          className="my-2 bg-gray-200 text-gray-700 pr-8 pl-8 text-sm hover:bg-gray-400 h-12 rounded-md align-bottom"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+          Enviar E-mails
+        </button>
+      </form>
     </div>
-  )
+  );
 }
---
+++ server/index.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const execa = require("execa");
+const fs = require("fs-extra");
+const multer = require("multer");
+const readXlsxFile = require("read-excel-file/node");
+const { format, sub } = require("date-fns");
+const { ptBR } = require("date-fns/locale");
+const cors = require("cors");
+
+const app = express();
+const router = express.Router();
+app.use(cors());
+app.use(router);
+app.listen(3005,()=>{console.log("Api Running in 3005 port")});
+
+const sgMail = require("@sendgrid/mail");
+
+const sendgridKrebsEngKey =
+"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";
+sgMail.setApiKey(sendgridKrebsEngKey);
+
+const upload = multer({ dest: "/tmp/" });
+
+
+
+const files = upload.fields([
+  { name: "pdf", maxCount: 1 },
+  { name: "excel", maxCount: 1 },
+]);
+
+async function cleanUpOldImages() {
+  try {
+    await execa("rm", [`/tmp/contra-cheque*.png`]);
+  } catch (err) {}
+}
+
+router.post("/", files, async (req, res) => {
+
+  try {
+    const { pdf, excel } = req.files;
+
+    await cleanUpOldImages();
+
+    await execa("convert", [pdf[0].path, `/tmp/contra-cheque.png`]);
+    console.log(excel[0]);
+    const rows = await readXlsxFile(excel[0].path/*, { sheet: 3 }*/);
+
+    let lineNumber = 0;
+
+    const mesPassado = sub(new Date(), { months: 1 });
+    const nomeDoMes = format(mesPassado, "MMMM", { locale: ptBR });
+    const ano = format(mesPassado, "yyyy");
+    const dataLegivel = `${nomeDoMes} de ${ano}`;
+
+    for (const row of rows) {
+      lineNumber++;
+      if (lineNumber === 1) {
+        continue;
+      }
+
+      const nome = row[1];// maybe number 2 
+      if (!nome || !nome.trim()) break;
+
+      const email = row[2];//maybe number 3
+      if (!email) continue;
+
+      const pagina = lineNumber - 2;
+
+      const file = await fs.readFile(`/tmp/contra-cheque-${pagina}.png`);
+      const attachment = file.toString("base64");
+      console.log(email);
+      const msg = {
+        from: "Gilberto Krebs <gilberto@krebseng.com.br>",
+        to: email,
+        subject: `Contra-cheque ${dataLegivel}`,
+        html: `<p>Olá, ${nome}</p><p>Neste e-mail você encontra o seu contra-cheque referente a ${dataLegivel}.</p>`,
+        attachments: [
+          {
+            content: attachment,
+            filename: "contra-cheque.png",
+            type: "image/png",
+            disposition: "attachment",
+          },
+        ],
+      };
+      await sgMail.send(msg);
+    }
+
+   res.send({message:"Emails Enviados com sucesso"});
+  } catch (err) {
+    console.error("-----------------------");
+    console.error(err);
+    console.error("-----------------------");
+    res.send(err);
+  }
+});
+
+module.exports = router;
