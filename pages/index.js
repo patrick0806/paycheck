f242e7c0e9fce0a14f40b1e66f37ae2a3cb3c0d7
Patrick da Silva Nicezi <patrickk0806@hotmail.com>
2021-07-13 21:41:45 -0300

    mostrando visualmente que está carregando

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,28 @@
 import Head from "next/head";
-import {useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 export default function Home() {
-  const {register, handleSubmit} = useForm();
-  const onSubmit = async (data) =>{
-
+  const { register, handleSubmit } = useForm();
+  const [processFiles, setProcessFiles] = useState(false);
+  const onSubmit = async (data) => {
     const formData = new FormData();
-    formData.append("pdf",data.pdf[0]);
-    formData.append("excel",data.excel[0]);
-    
-    const res = await fetch(
-      'http://localhost:3005',
-      {
-        body: formData,
-        method: 'POST'
-      }
-    )
+    formData.append("pdf", data.pdf[0]);
+    formData.append("excel", data.excel[0]);
+
+    const res = await fetch("http://localhost:3005", {
+      body: formData,
+      method: "POST",
+    });
 
     const response = await res.json();
-    if(response.message){
+    setProcessFiles(false);
+    if (response.message) {
       alert(response.message);
-    }else{
+    } else {
       alert("Houve uma falha ao enviar os emails");
     }
-
-  }
+  };
 
   return (
     <div className="w-full h-screen bg-white flex flex-col items-center justify-center">
@@ -34,9 +32,10 @@ export default function Home() {
         </label>
         <input
           id="pdf"
-        {...register('pdf',{required:true})}
+          {...register("pdf", { required: true })}
           type="file"
           name="pdf"
+          placeholder="Selecione o PDF"
           className="my-2 block border shadow-inner w-full text-gray-700 p-1 border-gray-400"
           required
         />
@@ -46,18 +45,56 @@ export default function Home() {
         <input
           id="excel"
           name="excel"
-          {...register('excel',{required:true})}
+          {...register("excel", { required: true })}
+          placeholder="Selecione a planilha"
           type="file"
           className="my-2 block border shadow-inner w-full text-gray-700 p-1 border-gray-400"
           required
         />
         <button
+          onClick={() => setProcessFiles(true)}
           type="submit"
-          className="my-2 bg-gray-200 text-gray-700 pr-8 pl-8 text-sm hover:bg-gray-400 h-12 rounded-md align-bottom"
+          className={
+            processFiles
+              ? `hidden`
+              : `my-2 bg-gray-200 text-gray-700 pr-8 pl-8 text-center text-sm hover:bg-gray-400 h-12 rounded-md flex align-center items-center`
+          }
         >
           Enviar E-mails
         </button>
+        <div
+          disabled
+          className={
+            processFiles
+              ? `my-2 bg-gray-200 text-gray-700 pr-8 pl-8 text-center text-sm hover:bg-gray-400 h-12 rounded-md flex align-center items-center`
+              : `hidden`
+          }
+        >
+          <svg
+            class="animate-spin -ml-1 mr-3 h-5 w-5 text-black"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+          Enviando Emails
+        </div>
       </form>
     </div>
   );
 }
+
+function SubmitButton() {}
